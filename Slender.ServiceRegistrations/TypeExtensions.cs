1991c69c3809704f4015d0fc9dbcc6c8cd7d2ead
James Roberts <James@Robertscom.com>
2022-09-20 19:05:53 +1000

    Initial Commit
    SR-0

---
+++ Slender.ServiceRegistrations/TypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Slender.ServiceRegistrations
+{
+
+    internal static class TypeExtensions
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        internal static Type GetTypeDefinition(this Type type)
+            => type.IsGenericType ? type.GetGenericTypeDefinition() : type;
+
+        internal static IEnumerable<Type> GetDirectInterfaces(this Type type)
+            => type.GetInterfaces().Except(type.GetInterfaces().SelectMany(i => i.GetInterfaces()));
+
+        #endregion Methods
+
+    }
+
+}
