a90f88bbc67f3cb1ba54efbee662adaf34c9f33a
James Roberts <James@Robertscom.com>
2022-11-26 22:29:05 +1100

    Add the ServiceScanVisitor
    SR-0

---
+++ Slender.ServiceRegistrations/Visitors/ServiceScanVisitor.cs
@@ -0,0 +1,34 @@
+﻿using Slender.AssemblyScanner;
+using System;
+
+namespace Slender.ServiceRegistrations.Visitors
+{
+
+    public class ServiceScanVisitor : AssemblyScanVisitor
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public Action<Type> OnServiceFound { get; set; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        protected override void VisitAbstract(Type abstractType)
+            => this.OnServiceFound?.Invoke(abstractType);
+
+        public override void VisitAssemblyScan(AssemblyScan scan)
+        {
+            foreach (var _Type in scan.Types)
+                this.VisitType(_Type);
+        }
+
+        protected override void VisitInterface(Type interfaceType)
+            => this.OnServiceFound?.Invoke(interfaceType);
+
+        #endregion Methods
+
+    }
+
+}
