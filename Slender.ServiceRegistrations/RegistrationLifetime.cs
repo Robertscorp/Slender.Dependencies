8a3ce65a232c949ab95d2c9e3a954c506f0e1e68
James Roberts <James@Robertscom.com>
2022-11-28 22:33:18 +1100

    Add the RegistrationLifetime smart enum
    SR-0

---
+++ Slender.ServiceRegistrations/RegistrationLifetime.cs
@@ -0,0 +1,37 @@
+﻿namespace Slender.ServiceRegistrations
+{
+
+    public class RegistrationLifetime
+    {
+
+        #region - - - - - - Constructors - - - - - -
+
+        private RegistrationLifetime(string name, bool allowImplementationInstances)
+        {
+            this.Name = name;
+            this.AllowImplementationInstances = allowImplementationInstances;
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public bool AllowImplementationInstances { get; }
+
+        public string Name { get; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        public static RegistrationLifetime Scoped() => new RegistrationLifetime("Scoped", false);
+
+        public static RegistrationLifetime Singleton() => new RegistrationLifetime("Singleton", true);
+
+        public static RegistrationLifetime Transient() => new RegistrationLifetime("Transient", false);
+
+        #endregion Methods
+
+    }
+
+}
