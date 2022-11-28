ffc6cfa648ba1d9c19c0d92bd5cfc2f71977878f
James Roberts <James@Robertscom.com>
2022-11-28 22:34:54 +1100

    Add the Registration context and RegistrationBehaviour service
    SR-0

---
+++ Slender.ServiceRegistrations/IRegistrationBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Slender.ServiceRegistrations
+{
+
+    public interface IRegistrationBehaviour
+    {
+
+        #region - - - - - - Methods - - - - - -
+
+        void AddImplementationType(RegistrationContext context, Type type);
+
+        void UpdateBehaviour(RegistrationContext context, IRegistrationBehaviour behaviour);
+
+        void UpdateImplementationFactory(RegistrationContext context, Func<object> implementationFactory);
+
+        void UpdateImplementationInstance(RegistrationContext context, object implementationInstance);
+
+        void UpdateLifetime(RegistrationContext context, RegistrationLifetime lifetime);
+
+        #endregion Methods
+
+    }
+
+}
---
+++ Slender.ServiceRegistrations/RegistrationContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slender.ServiceRegistrations
+{
+
+    public class RegistrationContext
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public IRegistrationBehaviour Behaviour { get; set; } = DefaultRegistrationBehaviour.Instance();
+
+        public Func<object> ImplementationFactory { get; set; }
+
+        public object ImplementationInstance { get; set; }
+
+        public List<Type> ImplementationTypes { get; } = new List<Type>();
+
+        public RegistrationLifetime Lifetime { get; set; }
+
+        #endregion Properties
+
+    }
+
+}
