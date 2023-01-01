5ecb2546fb769cb0d53b57914c0d8a70ed568e50
James Roberts <James@Robertscom.com>
2023-01-01 13:01:03 +1100

    Add ImplementationScanVisitor
    SR-0

---
+++ Slender.ServiceRegistrations/Visitors/ImplementationScanVisitor.cs
@@ -0,0 +1,29 @@
+﻿using Slender.AssemblyScanner;
+using System;
+using System.Collections.Generic;
+
+namespace Slender.ServiceRegistrations.Visitors
+{
+
+    internal class ImplementationScanVisitor : AssemblyScanVisitor
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public Action<Type, IEnumerable<Type>> OnServiceAndImplementationsFound { get; set; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        protected override void VisitAbstractAndImplementations(Type abstractType, IEnumerable<Type> implementationTypes)
+            => this.OnServiceAndImplementationsFound?.Invoke(abstractType, implementationTypes);
+
+        protected override void VisitInterfaceAndImplementations(Type interfaceType, IEnumerable<Type> implementationTypes)
+            => this.OnServiceAndImplementationsFound?.Invoke(interfaceType, implementationTypes);
+
+        #endregion Methods
+
+    }
+
+}
