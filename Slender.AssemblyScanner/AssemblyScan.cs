8aa801d8cb94ec1dbe8fc9fa8822a06cb9492c3a
James Roberts <James@Robertscom.com>
2023-01-01 12:52:15 +1100

    Redesign the AssemblyScan to no longer be immutable
    SR-0

--- Slender.AssemblyScanner/AssemblyScan.cs
+++ Slender.AssemblyScanner/AssemblyScan.cs
@@ -17,24 +17,48 @@ namespace Slender.AssemblyScanner
 
         #region - - - - - - Properties - - - - - -
 
-        public Type[] Types { get; private set; }
+        public Assembly[] Assemblies { get; private set; } = Array.Empty<Assembly>();
+
+        public Type[] Types { get; private set; } = Array.Empty<Type>();
 
         #endregion Properties
 
         #region - - - - - - Methods - - - - - -
 
-        public static AssemblyScan FromAssemblies(IEnumerable<Assembly> assemblies)
-            => assemblies is null
-                ? throw new ArgumentNullException(nameof(assemblies))
-                : new AssemblyScan { Types = assemblies.Where(a => a != null).SelectMany(a => a.GetTypes()).ToArray() };
+        public AssemblyScan AddAssemblies(IEnumerable<Assembly> assemblies)
+        {
+            assemblies = (assemblies ?? Enumerable.Empty<Assembly>()).Where(a => a != null).Except(this.Assemblies);
 
-        public static AssemblyScan FromAssemblies(Assembly assembly, params Assembly[] additionalAssembliesToScan)
-            => assembly is null
-                ? throw new ArgumentNullException(nameof(assembly))
-                : FromAssemblies(additionalAssembliesToScan.Where(a => a != null).Union(new[] { assembly }));
+            this.Types = this.Types.Union(assemblies.SelectMany(a => a.GetTypes())).ToArray();
+
+            return this;
+        }
+
+        public AssemblyScan AddAssemblies(Assembly assembly, params Assembly[] additionalAssemblies)
+            => this.AddAssemblies(new[] { assembly }.Union(additionalAssemblies));
+
+        public AssemblyScan AddAssembly(Assembly assembly)
+            => this.AddAssemblies(assembly);
+
+        public AssemblyScan AddAssemblyScan(AssemblyScan assemblyScan)
+        {
+            this.Assemblies = this.Assemblies.Union(assemblyScan.Assemblies).ToArray();
+            this.Types = this.Types.Union(assemblyScan.Types).ToArray();
+
+            return this;
+        }
+
+        public static AssemblyScan Empty()
+            => new AssemblyScan();
+
+        public static AssemblyScan FromAssemblies(IEnumerable<Assembly> assemblies)
+            => new AssemblyScan().AddAssemblies(assemblies);
+
+        public static AssemblyScan FromAssemblies(Assembly assembly, params Assembly[] additionalAssemblies)
+            => new AssemblyScan().AddAssemblies(assembly, additionalAssemblies);
 
         public static AssemblyScan FromAssembly(Assembly assembly)
-            => FromAssemblies(assembly);
+            => new AssemblyScan().AddAssembly(assembly);
 
         #endregion Methods
 
