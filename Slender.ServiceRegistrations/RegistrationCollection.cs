eff55a96ea8804b43beb36cec11ffa60f2c632ec
James Roberts <James@Robertscom.com>
2023-01-01 13:02:16 +1100

    Redesign to support implementation scanning
    Also added enumerating the registrations
    
    SR-0

--- Slender.ServiceRegistrations/RegistrationCollection.cs
+++ Slender.ServiceRegistrations/RegistrationCollection.cs
@@ -1,6 +1,7 @@
-﻿using Slender.ServiceRegistrations.Visitors;
+﻿using Slender.AssemblyScanner;
+using Slender.ServiceRegistrations.Visitors;
 using System;
-using System.Collections.Concurrent;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -8,15 +9,15 @@ using System.Reflection;
 namespace Slender.ServiceRegistrations
 {
 
-    public class RegistrationCollection
+    public class RegistrationCollection : IEnumerable<Registration>
     {
 
         #region - - - - - - Fields - - - - - -
 
-        private readonly List<Assembly> m_Assemblies = new List<Assembly>();
-        private readonly ConcurrentDictionary<Type, Registration> m_RegistrationsByType = new ConcurrentDictionary<Type, Registration>();
+        private readonly Dictionary<Type, IEnumerable<Type>> m_ImplementationsByType = new Dictionary<Type, IEnumerable<Type>>();
+        private readonly Dictionary<Type, Registration> m_RegistrationsByType = new Dictionary<Type, Registration>();
 
-        private ServiceScanVisitor m_ServiceScanVisitor;
+        private AssemblyScan m_AssemblyScan = AssemblyScan.Empty();
 
         #endregion Fields
 
@@ -28,20 +29,42 @@ namespace Slender.ServiceRegistrations
 
         #region - - - - - - Methods - - - - - -
 
-        public RegistrationCollection AddAssemblies(Assembly assembly, params Assembly[] additonalAssemblies)
-            => assembly is null
-                ? throw new ArgumentNullException(nameof(assembly))
-                : this.AddAssemblies(new[] { assembly }.Union(additonalAssemblies));
+        public RegistrationCollection AddAssemblies(Assembly assembly, params Assembly[] additionalAssemblies)
+        {
+            var _AssemblyScan = AssemblyScan.FromAssemblies(assembly, additionalAssemblies);
+
+            new ImplementationScanVisitor
+            {
+                OnServiceAndImplementationsFound = (service, implementations) =>
+                {
+                    if (this.m_RegistrationsByType.TryGetValue(service, out var _Registration))
+                        this.AddImplementations(_Registration, implementations);
+                    else if (this.m_ImplementationsByType.TryGetValue(service, out var _Implementations))
+                        this.m_ImplementationsByType[service] = _Implementations.Union(implementations);
+                    else
+                        this.m_ImplementationsByType.Add(service, implementations);
+                }
+            }.VisitAssemblyScan(_AssemblyScan);
+
+            _ = this.m_AssemblyScan.AddAssemblyScan(_AssemblyScan);
+
+            return this;
+        }
 
         public RegistrationCollection AddAssemblies(IEnumerable<Assembly> assemblies)
         {
-            this.m_Assemblies.AddRange(assemblies.Where(a => a != null));
-
+            _ = this.m_AssemblyScan.AddAssemblies(assemblies);
             return this;
         }
 
         public RegistrationCollection AddAssembly(Assembly assembly)
             => this.AddAssemblies(assembly);
+
+        private void AddImplementations(Registration registration, IEnumerable<Type> implementations)
+        {
+            foreach (var _Implementation in implementations)
+                _ = registration.AddImplementationType(_Implementation);
+        }
 
         public RegistrationCollection AddScopedService<TService>(Action<Registration> configurationAction = null)
             => this.AddScopedService(typeof(TService), configurationAction);
@@ -51,12 +74,20 @@ namespace Slender.ServiceRegistrations
 
         private RegistrationCollection AddService(Type type, RegistrationLifetime lifetime, Action<Registration> configurationAction)
         {
-            var _Registration
-                = this.m_RegistrationsByType.ContainsKey(type)
-                    ? throw new Exception($"{type.Name} has already been registered. Use {nameof(ConfigureService)} instead.")
-                    : this.m_RegistrationsByType.GetOrAdd(type, new Registration(type, lifetime));
+            if (this.m_RegistrationsByType.ContainsKey(type))
+                throw new Exception($"{type.Name} has already been registered. Use {nameof(ConfigureService)} instead.");
+
+            var _Registration = new Registration(type, lifetime);
+
+            this.m_RegistrationsByType.Add(type, _Registration);
 
             configurationAction?.Invoke(_Registration);
+
+            if (this.m_ImplementationsByType.TryGetValue(type, out var _Implementations))
+            {
+                this.AddImplementations(_Registration, _Implementations);
+                _ = this.m_ImplementationsByType.Remove(_Registration.ServiceType);
+            }
 
             return this;
         }
@@ -82,12 +113,20 @@ namespace Slender.ServiceRegistrations
             return this;
         }
 
+        IEnumerator<Registration> IEnumerable<Registration>.GetEnumerator()
+            => this.m_RegistrationsByType.Values.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => ((IEnumerable<Registration>)this).GetEnumerator();
+
         public RegistrationCollection ScanForUnregisteredServices(Action<Type> serviceRegistrationAction)
         {
-            this.m_ServiceScanVisitor = new ServiceScanVisitor
+            new ServiceScanVisitor
             {
                 OnServiceFound = t => { if (!this.m_RegistrationsByType.ContainsKey(t)) serviceRegistrationAction(t); }
-            };
+            }.VisitAssemblyScan(this.m_AssemblyScan);
+
+            this.m_AssemblyScan = AssemblyScan.Empty();
 
             return this;
         }
