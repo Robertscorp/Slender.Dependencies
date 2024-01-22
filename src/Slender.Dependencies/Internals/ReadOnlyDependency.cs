04a6523d086bb8da1f53170380a1b429206e09f5
James Roberts <James@Robertscom.com>
2024-01-22 19:51:10 +1100

    Add the read-only dependency and collection
    #34

---
+++ src/Slender.Dependencies/Internals/ReadOnlyDependency.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Slender.Dependencies.Internals
+{
+
+    internal class ReadOnlyDependency : IDependency
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly bool m_IsLocked;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public ReadOnlyDependency(IDependency dependency)
+        {
+            this.Dependency = dependency;
+            this.DependencyType = dependency.GetDependencyType();
+            dependency.AddToDependency(this);
+            this.m_IsLocked = true;
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public List<Type> Decorators { get; } = new List<Type>();
+
+        internal IDependency Dependency { get; }
+
+        public Type DependencyType { get; }
+
+        public List<object> Implementations { get; } = new List<object>();
+
+        public DependencyLifetime Lifetime { get; private set; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        void IDependency.AddDecorator(Type decoratorType)
+        {
+            if (!this.m_IsLocked)
+                this.Decorators.Add(decoratorType);
+        }
+
+        void IDependency.AddImplementation(object implementation)
+        {
+            if (!this.m_IsLocked)
+                this.Implementations.Add(implementation);
+        }
+
+        void IDependency.AddToDependency(IDependency dependency)
+            => throw new NotImplementedException();
+
+        Type IDependency.GetDependencyType()
+            => this.DependencyType;
+
+        void IDependency.SetLifetime(DependencyLifetime lifetime)
+        {
+            if (!this.m_IsLocked)
+                this.Lifetime = lifetime;
+        }
+
+        #endregion Methods
+
+    }
+
+}
---
+++ src/Slender.Dependencies/Internals/ReadOnlyDependencyCollection.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Slender.Dependencies.Internals
+{
+
+    internal class ReadOnlyDependencyCollection : IDependencyCollection
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly bool m_IsReadOnly;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public ReadOnlyDependencyCollection(IDependencyCollection dependencies)
+        {
+            dependencies.AddToDependencyCollection(this);
+            this.m_IsReadOnly = true;
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Properties - - - - - -
+
+        public List<ReadOnlyDependency> Dependencies { get; } = new List<ReadOnlyDependency>();
+
+        public List<Type> TransitiveDependencies { get; } = new List<Type>();
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        void IDependencyCollection.AddDependency(IDependency dependency)
+        {
+            if (!this.m_IsReadOnly)
+                this.Dependencies.Add(new ReadOnlyDependency(dependency));
+        }
+
+        IDependency IDependencyCollection.AddDependency(Type dependencyType)
+            => throw new NotImplementedException();
+
+        void IDependencyCollection.AddToDependencyCollection(IDependencyCollection dependencies)
+            => throw new NotImplementedException();
+
+        void IDependencyCollection.AddTransitiveDependency(Type transitiveDependencyType)
+        {
+            if (!this.m_IsReadOnly)
+                this.TransitiveDependencies.Add(transitiveDependencyType);
+        }
+
+        IDependency IDependencyCollection.GetDependency(Type dependencyType)
+            => this.Dependencies.FirstOrDefault(d => d.DependencyType == dependencyType);
+
+        #endregion Methods
+
+    }
+
+}
