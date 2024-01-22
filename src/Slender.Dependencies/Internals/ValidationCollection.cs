7e3a4b93e6af9ef45d6e0491a57ca3602c165625
James Roberts <James@Robertscom.com>
2024-01-22 19:46:58 +1100

    Add the validation collection and dependency
    #34

---
+++ src/Slender.Dependencies/Internals/ValidationCollection.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Slender.Dependencies.Internals
+{
+
+    internal class ValidationCollection : IDependencyCollection, IDependencyCollectionValidationOptions
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly List<ValidationDependency> m_Dependencies = new List<ValidationDependency>();
+        private readonly List<Type> m_TransitiveDependencies = new List<Type>();
+
+        private bool m_IgnoreInvalidImplementations;
+        private bool m_IgnoreMissingImplementations;
+        private bool m_IgnoreMissingLifetimes;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public ValidationCollection(IDependencyCollection dependencies)
+            => dependencies.AddToDependencyCollection(this);
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        public void AddDependency(IDependency dependency)
+            => this.m_Dependencies.Add(new ValidationDependency(dependency));
+
+        public void AddTransitiveDependency(Type transitiveDependencyType)
+        {
+            if (!this.m_TransitiveDependencies.Contains(transitiveDependencyType))
+                this.m_TransitiveDependencies.Add(transitiveDependencyType);
+        }
+
+        IDependency IDependencyCollection.AddDependency(Type dependencyType)
+            => throw new NotImplementedException();
+
+        IDependencyCollectionValidationOptions IDependencyCollectionValidationOptions.IgnoreInvalidImplementations()
+        {
+            this.m_IgnoreInvalidImplementations = true;
+            return this;
+        }
+
+        IDependencyCollectionValidationOptions IDependencyCollectionValidationOptions.IgnoreMissingImplementations()
+        {
+            this.m_IgnoreMissingImplementations = true;
+            return this;
+        }
+
+        IDependencyCollectionValidationOptions IDependencyCollectionValidationOptions.IgnoreMissingLifetimes()
+        {
+            this.m_IgnoreMissingLifetimes = true;
+            return this;
+        }
+
+        IDependencyCollectionValidationOptions IDependencyCollectionValidationOptions.ResolveTransitiveDependency(Type transitiveDependencyType)
+            => this.m_TransitiveDependencies.Remove(transitiveDependencyType)
+                ? this
+                : throw new InvalidOperationException($"'{transitiveDependencyType.Name}' is not a transitive dependency.");
+
+        void IDependencyCollection.AddToDependencyCollection(IDependencyCollection dependencies)
+            => throw new NotImplementedException();
+
+        IDependency IDependencyCollection.GetDependency(Type dependencyType)
+            => throw new NotImplementedException();
+
+        public void ThrowOnValidationFailure()
+        {
+            var _StringBuilder = new StringBuilder(64);
+
+            if (!this.m_IgnoreMissingLifetimes)
+            {
+                var _DependenciesWithoutLifetime = this.m_Dependencies.Where(d => d.HasNoLifetime()).ToList();
+                if (_DependenciesWithoutLifetime.Any())
+                {
+                    _ = _StringBuilder.AppendLine("The following dependencies don't have a lifetime:");
+
+                    foreach (var _Dependency in _DependenciesWithoutLifetime)
+                        _ = _StringBuilder.Append(" - ").AppendLine(_Dependency.GetDependencyType().Name);
+                }
+            }
+
+            if (!this.m_IgnoreMissingImplementations)
+            {
+                var _DependenciesWithoutImplementations = this.m_Dependencies.Where(d => d.HasNoImplementations()).ToList();
+                if (_DependenciesWithoutImplementations.Any())
+                {
+                    if (_StringBuilder.Length > 0)
+                        _ = _StringBuilder.AppendLine();
+
+                    _ = _StringBuilder.AppendLine("The following dependencies don't have any implementations:");
+
+                    foreach (var _Dependency in _DependenciesWithoutImplementations)
+                        _ = _StringBuilder.Append(" - ").AppendLine(_Dependency.GetDependencyType().Name);
+                }
+            }
+
+            if (!this.m_IgnoreInvalidImplementations)
+            {
+                var _DependenciesWithInvalidImplementations = this.m_Dependencies.Where(d => d.HasInvalidImplementations()).ToList();
+                if (_DependenciesWithInvalidImplementations.Any())
+                {
+                    if (_StringBuilder.Length > 0)
+                        _ = _StringBuilder.AppendLine();
+
+                    _ = _StringBuilder.AppendLine("The following dependencies have invalid implementations:");
+
+                    foreach (var _Dependency in _DependenciesWithInvalidImplementations)
+                        _ = _StringBuilder.Append(" - ").AppendLine(_Dependency.GetDependencyType().Name);
+                }
+            }
+
+            if (this.m_TransitiveDependencies.Any())
+            {
+                if (_StringBuilder.Length > 0)
+                    _ = _StringBuilder.AppendLine();
+
+                _ = _StringBuilder.AppendLine("The following transitive dependencies have not been resolved:");
+
+                foreach (var _TransitiveDependency in this.m_TransitiveDependencies)
+                    _ = _StringBuilder.Append(" - ").AppendLine(_TransitiveDependency.Name);
+            }
+
+            if (_StringBuilder.Length > 0)
+                throw new Exception(_StringBuilder.ToString());
+        }
+
+        #endregion Methods
+
+    }
+
+}
---
+++ src/Slender.Dependencies/Internals/ValidationDependency.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Slender.Dependencies.Internals
+{
+
+    internal class ValidationDependency : IDependency
+    {
+
+        #region - - - - - - Fields - - - - - -
+
+        private readonly List<Type> m_Decorators = new List<Type>();
+        private readonly Type m_DependencyType;
+        private readonly List<object> m_Implementations = new List<object>();
+
+        private DependencyLifetime m_Lifetime;
+
+        #endregion Fields
+
+        #region - - - - - - Constructors - - - - - -
+
+        public ValidationDependency(IDependency dependency)
+        {
+            this.m_DependencyType = dependency.GetDependencyType();
+            dependency.AddToDependency(this);
+        }
+
+        #endregion Constructors
+
+        #region - - - - - - Methods - - - - - -
+
+        public void AddDecorator(Type decoratorType)
+            => this.m_Decorators.Add(decoratorType);
+
+        public void AddImplementation(object implementation)
+            => this.m_Implementations.Add(implementation);
+
+        public Type GetDependencyType()
+            => this.m_DependencyType;
+
+        public bool HasInvalidImplementations()
+            => this.m_Lifetime != null && this.m_Implementations.Any(i => !this.m_Lifetime.SupportsImplementation(i));
+
+        public bool HasNoImplementations()
+            => !this.m_Implementations.Any();
+
+        public bool HasNoLifetime()
+            => this.m_Lifetime == null;
+
+        void IDependency.AddToDependency(IDependency dependency)
+            => throw new NotImplementedException();
+
+        public void SetLifetime(DependencyLifetime lifetime)
+            => this.m_Lifetime = lifetime;
+
+        #endregion Methods
+
+    }
+
+}
