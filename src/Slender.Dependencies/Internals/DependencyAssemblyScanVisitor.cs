38df2b6c55878aa755ab850243f681be32d81476
James Roberts <James@Robertscom.com>
2024-01-22 19:48:12 +1100

    Add the Dependency AssemblyScanVisitor
    #34

---
+++ src/Slender.Dependencies/Internals/DependencyAssemblyScanVisitor.cs
@@ -0,0 +1,45 @@
+﻿using Slender.AssemblyScanner;
+using System;
+using System.Collections.Generic;
+
+namespace Slender.Dependencies.Internals
+{
+
+    internal class DependencyAssemblyScanVisitor : AssemblyScanVisitor
+    {
+
+        #region - - - - - - Properties - - - - - -
+
+        public Action<Type> OnDependencyFound { get; set; }
+
+        public Action<Type, IEnumerable<Type>> OnDependencyImplementationsFound { get; set; }
+
+        public Action<Type> OnTypeFound { get; set; }
+
+        #endregion Properties
+
+        #region - - - - - - Methods - - - - - -
+
+        protected override void VisitAbstract(Type abstractType)
+            => this.OnDependencyFound.Invoke(abstractType);
+
+        protected override void VisitAbstractAndImplementations(Type abstractType, IEnumerable<Type> implementationTypes)
+            => this.OnDependencyImplementationsFound.Invoke(abstractType, implementationTypes);
+
+        protected override void VisitInterface(Type interfaceType)
+            => this.OnDependencyFound.Invoke(interfaceType);
+
+        protected override void VisitInterfaceAndImplementations(Type interfaceType, IEnumerable<Type> implementationTypes)
+            => this.OnDependencyImplementationsFound.Invoke(interfaceType, implementationTypes);
+
+        protected override void VisitType(Type type)
+        {
+            this.OnTypeFound.Invoke(type);
+            base.VisitType(type);
+        }
+
+        #endregion Methods
+
+    }
+
+}
