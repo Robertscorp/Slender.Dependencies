3ff76dd16d1212f7167c6a6fe7b663432643e2e2
James Roberts <James@Robertscom.com>
2024-01-28 11:56:47 +1100

    Add the DependencyProvider delegate
    #34

---
+++ src/Slender.Dependencies/Options/DependencyProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Slender.Dependencies.Options
+{
+
+    /// <summary>
+    /// Provides a new dependency instance.
+    /// </summary>
+    /// <param name="dependencyType">The type of dependency.</param>
+    /// <returns>A new dependency instance.</returns>
+    public delegate IDependency DependencyProvider(Type dependencyType);
+
+}
